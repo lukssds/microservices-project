e956cff17cf826048752ff01a3d459d5d582c949
Lucas Samuel <lukssds@gmail.com>
2021-12-13 20:30:25 -0300

    Configuring Zuul api gateway

---
+++ hr-api-gataway-zuul/src/test/java/com/iftm/ms/hrapigatewayzuul/HrApiGatawayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iftm.ms.hrapigatewayzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrApiGatawayZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
